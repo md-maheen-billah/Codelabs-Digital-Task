f78aecc63e3d51493cbc172e085a236a8c7a142a
md-maheen-billah <md.maheen.billah@icloud.com>
2024-07-03 14:44:01 +0200

    added banner

--- src/Banner/Banner.jsx
+++ src/Banner/Banner.jsx
@@ -1,1 +1,20 @@
+import banner from "../assets/banner.jpeg";
+import "./banner.css";
 
+const Banner = () => {
+  return (
+    <div className="h-[470px] relative mt-[40px]">
+      <div className="absolute z-10 inset-0 banner"></div>
+      <div
+        style={{
+          backgroundImage: `url(${banner})`,
+          backgroundPosition: "0% 0%",
+          borderRadius: "48px",
+        }}
+        className="bg-cover absolute inset-0 h-[470px]"
+      ></div>
+    </div>
+  );
+};
+
+export default Banner;
